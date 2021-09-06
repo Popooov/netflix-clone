2d31b42610e9fea17130f1e533c3c48c3212ef9a
Alexander <31711254+Popooov@users.noreply.github.com>
2021-09-06 23:58:15 +0100

    Building the Jumbotron Component

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,11 @@
+export default function Jumbotron({ children, direction = 'row', ...restProps }) {
+    return (
+        <Item {...restProps}>
+            <Inner direction={direction}>{children}</Inner>
+        </Item>
+    )
+}
+
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+    return <Container {...restProps}>{children}</Container>
+}

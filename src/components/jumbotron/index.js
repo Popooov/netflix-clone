0bac1100a900ab72ddc3a6a7dd60bf74c5f1ec9f
Alexander <31711254+Popooov@users.noreply.github.com>
2021-09-08 00:29:52 +0100

    Styling the Jumbotron Component
    Netflix in React JS

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,4 +1,4 @@
-import { Container, Item, Inner } from "./styles/jumbotron"
+import { Container, Item, Inner, Title, SubTitle } from "./styles/jumbotron"
 
 export default function Jumbotron({ children, direction = 'row', ...restProps }) {
     return (
@@ -10,4 +10,12 @@ export default function Jumbotron({ children, direction = 'row', ...restProps })
 
 Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
     return <Container {...restProps}>{children}</Container>
+}
+
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+    return <Title {...restProps}>{children}</Title>
+}
+
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+    return <SubTitle {...restProps}>{children}</SubTitle>
 }

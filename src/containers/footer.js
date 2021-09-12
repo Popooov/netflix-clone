6fe62cb2d36f33df24a34c2374dd5fb52c318fc2
Alexander <31711254+Popooov@users.noreply.github.com>
2021-09-12 23:59:33 +0100

    Building the Footer Component
    Netflix in React JS

---
+++ src/containers/footer.js
@@ -0,0 +1,9 @@
+import { Footer } from "../components"
+
+export function FooterContainer() {
+    return (
+        <Footer>
+            <p>Hello</p>
+        </Footer>
+    )
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,11 @@
 import { JumbotronContainer } from "../containers/jumbotron"
+import { FooterContainer } from "../containers/footer"
 
 export default function Home() {
     return (
-        <JumbotronContainer />
+        <>
+            <JumbotronContainer />
+            <FooterContainer />
+        </>
     )
 }

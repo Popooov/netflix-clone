ad2f83c0d1bf056315ede33cda89cac03c796a39
Alexander <31711254+Popooov@users.noreply.github.com>
2021-09-13 17:56:22 +0100

    Building the Accordion Component
    Netflix in React JS

---
+++ src/containers/faqs.js
@@ -0,0 +1,16 @@
+import { Accordion } from "../components"
+import faqsData from '../fixtures/faqs.json'
+
+export function FaqsContainer() {
+    return (
+        <Accordion>
+            <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+            {faqsData.map(({ id, header, body }) => (
+                <Accordion.Item key={id}>
+                    <Accordion.Header>{header}</Accordion.Header>
+                    <Accordion.Body>{body}</Accordion.Body>
+                </Accordion.Item>
+            ))}
+        </Accordion>
+    )
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,12 @@
 import { JumbotronContainer } from "../containers/jumbotron"
 import { FooterContainer } from "../containers/footer"
+import { FaqsContainer } from "../containers/faqs"
 
 export default function Home() {
     return (
         <>
             <JumbotronContainer />
+            <FaqsContainer />
             <FooterContainer />
         </>
     )

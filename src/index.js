ba595374f6f2b8bfbfe8114970811d04cb5f377f
Alexander <31711254+Popooov@users.noreply.github.com>
2022-02-08 21:55:06 +0000

    Clear index.html file, hook the app up to firebase
    Netflix in ReactJS

--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { GlobalStyles } from './global-styles';
 import { App } from './app';
+import { FirebaseContext } from './context/firebase';
+
+const config = {
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+}
+
+const firebase = window.firebase.initializeApp(config)
 
 ReactDOM.render(
-    <React.StrictMode>
-        <GlobalStyles />
-        <App />
-    </React.StrictMode>,
+    <>
+        <FirebaseContext.Provider value={{ firebase }}>
+            <GlobalStyles />
+            <App />
+        </FirebaseContext.Provider>
+    </>,
     document.getElementById('root')
 );

762b621047b93912fdc9faa702766f0d2f92fc60
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2022-03-08 23:50:39 +0000

    delete React imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import { Route } from 'react-router-dom'
 import * as ROUTES from './constants/routes'
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
 import { GlobalStyles } from './global-styles'
 import { App } from './app'

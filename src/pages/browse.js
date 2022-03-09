f61951897e4022430165d0a30ceaa9c39970bcd5
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2022-03-09 23:33:09 +0000

    create a data selection map utility

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,9 +1,11 @@
 import { BrowseContainer } from "../containers/browse"
 import { useContent } from '../hooks'
+import { selectionMap } from '../utils'
 
 export default function Browse() {
     const { series } = useContent('series')
     const { films } = useContent('films')
+    const slides = selectionMap({ series, films })
 
     console.log(series)
     console.log(films)
